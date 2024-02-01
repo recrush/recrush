06cba1e7466302b395505f536db908ae5112f83f
Chojan Shang <psiace@apache.org>
2024-02-01 16:41:07 +0800

    feat(cache): add a basic modular cache impl
    Signed-off-by: Chojan Shang <psiace@apache.org>

--- src/recrush/src/lib.rs
+++ src/recrush/src/lib.rs
@@ -1,14 +1,45 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+mod cache;
+mod policy;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub use cache::{ModularCache, TtlCache};
+pub use policy::{EvictionPolicy, InsertionPolicy};
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[cfg(feature = "async")]
+pub use crate::cache::ConcurrentCache;
+
+pub mod preconfig {
+    use super::policy::eviction::LruEvictionPolicy;
+    use super::policy::insertion::AlwaysInsertPolicy;
+    use super::ModularCache;
+
+    pub type LruCache<K, V> = ModularCache<K, V, AlwaysInsertPolicy, LruEvictionPolicy<K>>;
+    pub type TtlLruCache<K, V> = super::TtlCache<K, V, AlwaysInsertPolicy, LruEvictionPolicy<K>>;
+
+    #[cfg(feature = "async")]
+    pub mod concurrent {
+        use super::LruCache as SingleThreadLruCache;
+        use super::TtlLruCache as SingleThreadTtlCache;
+        use crate::cache::ConcurrentCache;
+        use std::{hash::Hash, time::Duration};
+
+        pub type LruCache<K, V> = ConcurrentCache<SingleThreadLruCache<K, V>>;
+
+        pub fn new_lru_cache<K, V>(maximum_size: usize) -> LruCache<K, V>
+        where
+            K: Hash + Eq + Clone + std::fmt::Debug,
+            V: Clone,
+        {
+            LruCache::new(SingleThreadLruCache::new(maximum_size))
+        }
+
+        pub type TtlLruCache<K, V> = ConcurrentCache<SingleThreadTtlCache<K, V>>;
+
+        pub fn new_ttl_cache<K, V>(maximum_size: usize, ttl: Duration) -> TtlLruCache<K, V>
+        where
+            K: Hash + Eq + Clone + std::fmt::Debug,
+            V: Clone,
+        {
+            TtlLruCache::new(SingleThreadTtlCache::new(maximum_size, ttl))
+        }
     }
 }
---
+++ src/recrush/src/policy/insert_always.rs
@@ -0,0 +1,53 @@
+use std::borrow::Borrow;
+use std::hash::Hash;
+
+use crate::policy::InsertionPolicy;
+
+#[derive(Default)]
+pub struct AlwaysInsertPolicy {}
+
+impl<K> InsertionPolicy<K> for AlwaysInsertPolicy {
+    fn should_add(&mut self, _key: &K) -> bool {
+        true
+    }
+
+    fn should_replace(&mut self, _candidate: &K, _victim: &K) -> bool {
+        true
+    }
+
+    fn on_cache_hit<Q: ?Sized>(&mut self, _key: &Q)
+    where
+        K: Borrow<Q>,
+        Q: Hash + Eq,
+    {
+    }
+    fn on_cache_miss<Q: ?Sized>(&mut self, _key: &Q)
+    where
+        K: Borrow<Q>,
+        Q: Hash + Eq,
+    {
+    }
+
+    fn clear(&mut self) {}
+
+    fn invalidate<Q: ?Sized>(&mut self, _key: &Q)
+    where
+        K: Borrow<Q>,
+        Q: Hash + Eq,
+    {
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn always_returns_true() {
+        let mut policy = AlwaysInsertPolicy::default();
+
+        for i in 0..10 {
+            assert!(policy.should_add(&i))
+        }
+    }
+}

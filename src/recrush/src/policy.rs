16ffee46f78c169f3b3bde86d076c8d5796abacc
Chojan Shang <psiace@apache.org>
2024-02-01 16:45:38 +0800

    feat(cache): add a basic modular cache impl (#3)
    * feat(cache): add a basic modular cache impl
    
    Signed-off-by: Chojan Shang <psiace@apache.org>
    
    * chore: manage workspace-hack
    
    Signed-off-by: Chojan Shang <psiace@apache.org>
    
    ---------
    
    Signed-off-by: Chojan Shang <psiace@apache.org>

---
+++ src/recrush/src/policy.rs
@@ -0,0 +1,54 @@
+mod evict_lru;
+mod insert_always;
+
+pub mod insertion {
+    pub use super::insert_always::AlwaysInsertPolicy;
+}
+
+pub mod eviction {
+    pub use super::evict_lru::LruEvictionPolicy;
+}
+
+use std::borrow::Borrow;
+use std::hash::Hash;
+
+pub trait InsertionPolicy<K> {
+    fn should_add(&mut self, key: &K) -> bool;
+    fn should_replace(&mut self, candidate: &K, victim: &K) -> bool;
+
+    fn on_cache_hit<Q: ?Sized>(&mut self, key: &Q)
+    where
+        K: Borrow<Q>,
+        Q: Hash + Eq;
+
+    fn on_cache_miss<Q: ?Sized>(&mut self, key: &Q)
+    where
+        K: Borrow<Q>,
+        Q: Hash + Eq;
+
+    fn clear(&mut self);
+
+    fn invalidate<Q: ?Sized>(&mut self, key: &Q)
+    where
+        K: Borrow<Q>,
+        Q: Hash + Eq;
+}
+
+pub trait EvictionPolicy<K> {
+    fn get_victim(&mut self) -> Option<K>;
+
+    fn on_eviction(&mut self, key: &K);
+    fn on_insert(&mut self, key: &K);
+    fn on_update(&mut self, key: &K);
+    fn on_cache_hit<Q: ?Sized>(&mut self, key: &Q)
+    where
+        K: Borrow<Q>,
+        Q: Hash + Eq;
+
+    fn clear(&mut self);
+
+    fn invalidate<Q: ?Sized>(&mut self, key: &Q)
+    where
+        K: Borrow<Q>,
+        Q: Hash + Eq;
+}
